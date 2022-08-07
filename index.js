7341245b2b0e20da0a49f5ed8c62e5411674ff40
Daniel Macharia <danielhuios@gmail.com>
2022-08-07 10:31:17 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+var a, b;
+
+function add() {
+  return a + b;
+}
+
+function subtract() {
+  return a - b;
+}
+
+function multiply() {
+  return a * b;
+}
+
+function divide() {
+  return a / b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(a) {
+  a++;
+  return a;
+}
+
+function decrement(a) {
+  a--;
+  return a;
+}
+
+function makeInt(n) {
+  parseInt(n);
+  return n;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
